fdf0509c11e8abf2b89aba717a8af6bf7a81e168
MingTaoChen <mail@mittacy.com>
2020-01-02 10:34:04 +0800

    删除矩形填充功能

--- main.c
+++ main.c
@@ -21,7 +21,6 @@ const char g_szClassName[] = "myWindowClass";
 #define LINE_BTN 31
 #define RECT_BTN 32
 #define CRICLE_BTN 33
-#define FILLAREA_BTN 34
 #define CLEAN_BTN 35
 // 画笔类型
 #define SOLID_BTN PS_SOLID
@@ -261,17 +260,11 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
                 WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
                 190 /*X坐标*/, 100 /*Y坐标*/, 40 /*宽度*/, 30/*高度*/,
                 hwnd, (HMENU)CRICLE_BTN, (HINSTANCE)GetWindowLong(hwnd, GWL_HINSTANCE), NULL);
-            HWND fillarea_btn = CreateWindow(
-                TEXT("BUTTON"), //按钮控件的类名
-                TEXT("填充"),
-                WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
-                240 /*X坐标*/, 100 /*Y坐标*/, 40 /*宽度*/, 30/*高度*/,
-                hwnd, (HMENU)FILLAREA_BTN, (HINSTANCE)GetWindowLong(hwnd, GWL_HINSTANCE), NULL);
             HWND clean_btn = CreateWindow(
                 TEXT("BUTTON"), //按钮控件的类名
                 TEXT("擦除"),
                 WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
-                290 /*X坐标*/, 100 /*Y坐标*/, 40 /*宽度*/, 30/*高度*/,
+                240 /*X坐标*/, 100 /*Y坐标*/, 40 /*宽度*/, 30/*高度*/,
                 hwnd, (HMENU)CLEAN_BTN, (HINSTANCE)GetWindowLong(hwnd, GWL_HINSTANCE), NULL);
             // 创建画笔类型
             HWND pen_style = CreateWindow(TEXT("static"), 
@@ -334,8 +327,6 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
                     changeDrawWhat(RECT_BTN);break;
                 case CRICLE_BTN:
                     changeDrawWhat(CRICLE_BTN);break;
-                case FILLAREA_BTN:
-                    changeDrawWhat(FILLAREA_BTN);break;
                 case CLEAN_BTN:
                     changeFillColor(FILL_BTN_NULL);
                     changeDrawWhat(CLEAN_BTN);break;
@@ -382,8 +373,6 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
                         CreateRectangle(hdc, start_x, start_y, end_x, end_y);break;
                     case CRICLE_BTN:
                         CreateEllipse(hdc, start_x, start_y, end_x, end_y);break;
-                    case FILLAREA_BTN:
-                        CreateRectangle(hdc, PS_NULL, start_x, start_y, end_x, end_y);break;
                     case CLEAN_BTN:
                         CreateRectangle(hdc, PS_NULL, start_x, start_y, end_x, end_y);break;
                     default:
